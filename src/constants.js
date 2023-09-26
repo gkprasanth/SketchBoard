af9eec2092505402184549a181100a476254bc23
gkprasanth <gkpc2004@gmail.com>
2023-09-26 18:53:44 +0530

    Toolbox Done

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+    BLACK: 'black',
+    RED: 'red',
+    GREEN: 'green',
+    BLUE: 'blue',
+    ORANGE: 'orange',
+    YELLOW: 'yellow',
+    WHITE: 'white'
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Menu from "@/components/Menu";
+import ToolBox from "@/components/Toolbox";
 
 export default function Home() {
   return (
     <>
     <Menu/>
+    <ToolBox/>
     </>
   )
 }
