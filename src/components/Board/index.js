4075b594752bb8aef54a37df4a8c4d1dfa73ea51
gkprasanth <gkpc2004@gmail.com>
2023-09-30 23:03:38 +0530

    Sever Done

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,113 +1,127 @@
-import { MENU_ITEMS } from '@/constants';
-import React, { useEffect, useLayoutEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { menuItemClick, actionItemClick } from '@/slice/menuSlice'
+import { useEffect, useLayoutEffect, useRef } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+
+import { MENU_ITEMS } from "@/constants";
+import { actionItemClick } from '@/slice/menuSlice'
+
+import { socket } from "@/socket";
+
 const Board = () => {
     const dispatch = useDispatch()
-
-    const canvasRef = useRef(null);
-    const shouldDraw = useRef(false);
-    const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
-    const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
-    const drawHistory = useRef([]);
+    const canvasRef = useRef(null)
+    const drawHistory = useRef([])
     const historyPointer = useRef(0)
+    const shouldDraw = useRef(false)
+    const {activeMenuItem, actionMenuItem} = useSelector((state) => state.menu)
+    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
 
 
     useEffect(() => {
-        if (!canvasRef.current) return;
+        if (!canvasRef.current) return
         const canvas = canvasRef.current;
-
-        const context = canvas.getContext('2d');
+        const context = canvas.getContext('2d')
 
         if (actionMenuItem === MENU_ITEMS.DOWNLOAD) {
             const URL = canvas.toDataURL()
             const anchor = document.createElement('a')
             anchor.href = URL
-            anchor.download = 'sketch.png'
+            anchor.download = 'sketch.jpg'
             anchor.click()
-            console.log(URL)
-        } else if (actionMenuItem === MENU_ITEMS.UNDO || actionMenuItem === MENU_ITEMS.REDO) {
-            if (historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO) historyPointer.current -= 1
-            if (historyPointer.current < drawHistory.current.length - 1 && actionMenuItem === MENU_ITEMS.REDO) historyPointer.current += 1
+        } else  if (actionMenuItem === MENU_ITEMS.UNDO || actionMenuItem === MENU_ITEMS.REDO) {
+            if(historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO) historyPointer.current -= 1
+            if(historyPointer.current < drawHistory.current.length - 1 && actionMenuItem === MENU_ITEMS.REDO) historyPointer.current += 1
             const imageData = drawHistory.current[historyPointer.current]
             context.putImageData(imageData, 0, 0)
         }
         dispatch(actionItemClick(null))
-
-    }, [actionMenuItem, activeMenuItem])
+    }, [actionMenuItem, dispatch])
 
     useEffect(() => {
-        if (!canvasRef.current) return;
+        if (!canvasRef.current) return
         const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
+        const context = canvas.getContext('2d')
 
-        const changeConfig = () => {
-            context.strokeStyle = color;
-            context.lineWidth = size;
-        };
+        const changeConfig = (color, size) => {
+            context.strokeStyle = color
+            context.lineWidth = size
+        }
 
-        changeConfig();
-    }, [color, size, activeMenuItem]); // Include activeMenuItem in the dependency array
+        const handleChangeConfig = (config) => {
+            console.log("config", config)
+            changeConfig(config.color, config.size)
+        }
+        changeConfig(color, size)
+        socket.on('changeConfig', handleChangeConfig)
 
+        return () => {
+            socket.off('changeConfig', handleChangeConfig)
+        }
+    }, [color, size])
 
-    //Before browser paint
+    // before browser pain
     useLayoutEffect(() => {
-        if (!canvasRef.current) return;
+        if (!canvasRef.current) return
         const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
+        const context = canvas.getContext('2d')
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
 
-
-        const BeginPath = (x, y) => {
-            context.beginPath(x, y);
-            // context.moveTo(x, y);
+        const beginPath = (x, y) => {
+            context.beginPath()
+            context.moveTo(x, y)
         }
 
         const drawLine = (x, y) => {
-            context.lineTo(x, y);
-            context.stroke();
+            context.lineTo(x, y)
+            context.stroke()
+        }
+        const handleMouseDown = (e) => {
+            shouldDraw.current = true
+            beginPath(e.clientX, e.clientY)
+            socket.emit('beginPath', {x: e.clientX, y: e.clientY})
         }
 
-        const handleMouseDown = (e) => {
-            shouldDraw.current = true;
-            BeginPath(e.clientX, e.clientY)
-        };
+        const handleMouseMove = (e) => {
+            if (!shouldDraw.current) return
+            drawLine(e.clientX, e.clientY)
+            socket.emit('drawLine', {x: e.clientX, y: e.clientY})
+        }
 
-        const handleMouseUp = () => {
-            shouldDraw.current = false;
-            // context.closePath(); // Close the path on mouse up
+        const handleMouseUp = (e) => {
+            shouldDraw.current = false
             const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
             drawHistory.current.push(imageData)
             historyPointer.current = drawHistory.current.length - 1
-        };
+        }
 
-        const handleMouseMove = (e) => {
-            if (!shouldDraw.current) return;
-            drawLine(e.clientX, e.clientY)
-        };
+        const handleBeginPath = (path) => {
+            beginPath(path.x, path.y)
+        }
 
+        const handleDrawLine = (path) => {
+            drawLine(path.x, path.y)
+        }
 
-        canvas.addEventListener('mousedown', handleMouseDown);
-        canvas.addEventListener('mousemove', handleMouseMove);
-        canvas.addEventListener('mouseup', handleMouseUp);
+        canvas.addEventListener('mousedown', handleMouseDown)
+        canvas.addEventListener('mousemove', handleMouseMove)
+        canvas.addEventListener('mouseup', handleMouseUp)
+
+        socket.on('beginPath', handleBeginPath)
+        socket.on('drawLine', handleDrawLine)
 
         return () => {
+            canvas.removeEventListener('mousedown', handleMouseDown)
+            canvas.removeEventListener('mousemove', handleMouseMove)
+            canvas.removeEventListener('mouseup', handleMouseUp)
 
-            canvas.removeEventListener('mousedown', handleMouseDown);
-            canvas.removeEventListener('mousemove', handleMouseMove);
-            canvas.removeEventListener('mouseup', handleMouseUp);
-        };
-    }, []); // No dependencies needed here
+            socket.off('beginPath', handleBeginPath)
+            socket.off('drawLine', handleDrawLine)
+        }
+    }, [])
 
-
-
-    return (
-        <>
-            <canvas ref={canvasRef}></canvas>
-        </>
-    );
-};
+    return (<canvas ref={canvasRef}></canvas>
+    )
+}
 
 export default Board;
